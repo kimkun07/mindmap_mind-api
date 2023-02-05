ebf609c7059565ef2423a4705b016f40434bc691
kimkun07 <kimkun07@naver.com>
2023-02-05 20:32:52 +0900

    v1.0.0 -완성된  interface. NodeObj.id 가 Nullable
    - 축약된 interface 대신 mind-elixir-core 에서 사용하는 그대로 사용
    - NodeObj.id 가 nullable하게 수정

--- index.ts
+++ index.ts
@@ -1,18 +1,28 @@
+// mind-elixir-core/src/index.ts 참고
+
 export interface MindResponse {
   status: string;
-  rawNodeData: RawNodeData;
+  rawNodeData: NodeObj;
 }
-export interface RawNodeData {
+export interface NodeObj {
   topic: string;
-  children: RawNodeData[];
-  style?: NodeStyle;
+  parent?: NodeObj;
+  style?: StyleObj;
+  id?: string; // original mind-elixir-core 에서는 not nullable
+  children?: NodeObj[];
+  tags?: string[];
+  icons?: string[]; // -> <span>${encodeHTML(icon)}</span>
+  hyperLink?: string;
+  expanded?: boolean;
+  direction?: number;
+  root?: boolean;
 }
-export interface NodeStyle {
-  color?: string;
-  background?: string;
-  fontSize?: string;
-  fontWeight?: string;
-  border?: string;
+export interface StyleObj {
+  color?: string; // default: 'inherit'
+  background?: string; // default: '#F6F6F6'
+  fontSize?: string; // default: '15'
+  fontWeight?: string; // default: 'normal'
+  border?: string; // default: 'none'
   borderWidth?: string;
   borderStyle?: string;
   borderColor?: string;
